49e9c85910ffb91f2c07539bdd896efd3a3aa7a7
Jordan Aranda Tejada <Jordan@MacBook-Pro-de-Jordan.local>
2016-03-16 20:48:09 +0100

    Se sigue con el panel de propiedad

--- src/views/items/PropiedadesPanel.java
+++ src/views/items/PropiedadesPanel.java
@@ -4,7 +4,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,32 +11,38 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import entities.Propiedad;
+import entities.Provincia;
+
 public class PropiedadesPanel extends JPanel {
 
 	private static final long	serialVersionUID	= 5131759915996741639L;
 
-	public static int			MODE_EDIT			= 0;
-	public static int			MODE_VIEW			= 1;
+	public static int			MODE_NEW			= 0;
+	public static int			MODE_EDIT			= 1;
+	public static int			MODE_VIEW			= 2;
 
+	private JLabel				lblId;
 	private JTextField			txtId;
 	private JTextField			txtNombre;
-	private JTextField			txtAddress;
+	private JTextArea			txtDescripcion;
+	private JTextField			txtDireccion;
+	private JComboBox<String>	cboProvincia;
 	private JTextField			txtLatitud;
 	private JTextField			txtLongitud;
 	private JTextField			txtPrecio;
 	private JTextField			txtArea;
-	private JButton				btnCancelar;
-	private JButton				btnGuardar;
 
-	public PropiedadesPanel(int mode) {
+	public PropiedadesPanel(int mode, Propiedad propiedad) {
+
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
 
-		JLabel lblId = new JLabel("ID");
+		lblId = new JLabel("ID");
 		GridBagConstraints gbc_lblId = new GridBagConstraints();
 		gbc_lblId.insets = new Insets(0, 0, 5, 5);
 		gbc_lblId.anchor = GridBagConstraints.WEST;
@@ -88,7 +93,7 @@ public class PropiedadesPanel extends JPanel {
 		gbc_scrollPane.gridy = 3;
 		add(scrollPane, gbc_scrollPane);
 
-		JTextArea txtDescripcion = new JTextArea();
+		txtDescripcion = new JTextArea();
 		scrollPane.setViewportView(txtDescripcion);
 
 		JLabel lblDireccin = new JLabel("Dirección");
@@ -99,14 +104,14 @@ public class PropiedadesPanel extends JPanel {
 		gbc_lblDireccin.gridy = 4;
 		add(lblDireccin, gbc_lblDireccin);
 
-		txtAddress = new JTextField();
+		txtDireccion = new JTextField();
 		GridBagConstraints gbc_txtAddress = new GridBagConstraints();
 		gbc_txtAddress.insets = new Insets(0, 0, 5, 5);
 		gbc_txtAddress.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtAddress.gridx = 2;
 		gbc_txtAddress.gridy = 4;
-		add(txtAddress, gbc_txtAddress);
-		txtAddress.setColumns(10);
+		add(txtDireccion, gbc_txtAddress);
+		txtDireccion.setColumns(10);
 
 		JLabel lblProvincia = new JLabel("Provincia");
 		GridBagConstraints gbc_lblProvincia = new GridBagConstraints();
@@ -116,7 +121,7 @@ public class PropiedadesPanel extends JPanel {
 		gbc_lblProvincia.gridy = 5;
 		add(lblProvincia, gbc_lblProvincia);
 
-		JComboBox cboProvincia = new JComboBox();
+		cboProvincia = new JComboBox<String>();
 		GridBagConstraints gbc_cboProvincia = new GridBagConstraints();
 		gbc_cboProvincia.insets = new Insets(0, 0, 5, 5);
 		gbc_cboProvincia.fill = GridBagConstraints.HORIZONTAL;
@@ -192,21 +197,53 @@ public class PropiedadesPanel extends JPanel {
 		add(txtArea, gbc_txtArea);
 		txtArea.setColumns(10);
 
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.anchor = GridBagConstraints.EAST;
-		gbc_panel.gridwidth = 2;
-		gbc_panel.insets = new Insets(0, 0, 5, 5);
-		gbc_panel.fill = GridBagConstraints.VERTICAL;
-		gbc_panel.gridx = 1;
-		gbc_panel.gridy = 11;
-		add(panel, gbc_panel);
-
-		btnCancelar = new JButton("Cancelar");
-		panel.add(btnCancelar);
-
-		btnGuardar = new JButton("Guardar");
-		panel.add(btnGuardar);
+		setMode(mode, propiedad);
 	}
 
+	private void setMode(int mode, Propiedad propiedad) {
+
+		// Se habilitan o inhabilitan los controles
+		this.txtNombre.setEditable(mode != MODE_VIEW);
+		this.txtDescripcion.setEditable(mode != MODE_VIEW);
+		this.txtDireccion.setEditable(mode != MODE_VIEW);
+		this.cboProvincia.setEditable(mode != MODE_VIEW);
+		this.txtLatitud.setEditable(mode != MODE_VIEW);
+		this.txtLongitud.setEditable(mode != MODE_VIEW);
+		this.txtPrecio.setEditable(mode != MODE_VIEW);
+		this.txtArea.setEditable(mode != MODE_VIEW);
+
+		// Se cargan los datos si estamos en modo editable
+		if (mode != MODE_NEW) {
+			this.txtId.setText(propiedad.getId());
+			this.txtNombre.setText(propiedad.getNombre());
+			this.txtDescripcion.setText(propiedad.getDescripcion());
+			this.txtDireccion.setText(propiedad.getDireccion());
+			this.cboProvincia.setSelectedItem(propiedad.getProvincia());
+			this.txtLatitud.setText(Float.toString(propiedad.getLatitud()));
+			this.txtLongitud.setText(Float.toString(propiedad.getLongitud()));
+			this.txtPrecio.setText(Double.toString(propiedad.getPrecio()));
+			this.txtArea.setText(Double.toString(propiedad.getArea()));
+		} else {
+			this.txtId.setText(Integer.toString(0));
+			this.lblId.setVisible(false);
+			this.txtId.setVisible(false);
+		}
+	}
+
+	private Propiedad obtenerPropiedad() {
+
+		// Se carga la propiedad con los valores del formulario y la devolvemos
+		Propiedad propiedad = new Propiedad();
+		propiedad.setId(Integer.parseInt(txtId.getText().trim()));
+		propiedad.setNombre(txtNombre.getText().trim());
+		propiedad.setDescripcion(txtDescripcion.getText().trim());
+		propiedad.setDireccion(txtDireccion.getText().trim());
+		propiedad.setProvincia((Provincia) cboProvincia.getSelectedItem());
+		propiedad.setLatitud(Float.parseFloat(txtLatitud.getText().trim()));
+		propiedad.setLatitud(Float.parseFloat(txtLongitud.getText().trim()));
+		propiedad.setPrecio(Double.parseDouble(txtPrecio.getText().trim()));
+		propiedad.setArea(Double.parseDouble(txtArea.getText().trim()));
+
+		return propiedad;
+	}
 }
